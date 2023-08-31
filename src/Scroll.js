5c22224b58b50da349c5d17abf1f8c6951021e7b
Mayur <mayur.gupta@girnarsoft.co.in>
2023-08-31 16:11:04 +0530

    added deploy and predeploy prop in packageJson

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ const Scroll = (props) => {
   console.log(props);
   return (
     <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
-      {props.children}fd
+      {props.children}
     </div>
   );
 }
